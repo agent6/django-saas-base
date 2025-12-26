5163bc2e2c331695e2e5603cd7a0d0f33016b3e6
agent6 <erik.pope@gmail.com>
2025-12-26 14:27:26 -0700

    commit-fix-email

--- backend/adminpanel/forms.py
+++ backend/adminpanel/forms.py
@@ -1,4 +1,7 @@
+from email.utils import parseaddr
+
 from django import forms
+from django.conf import settings as django_settings
 from django.contrib.auth.models import Group
 
 from core.forms import apply_tailwind_classes
@@ -27,6 +30,17 @@ class SiteSettingsForm(forms.ModelForm):
         self.fields["email_use_tls"].widget.attrs[
             "class"
         ] = "h-4 w-4 rounded border-slate-300 text-slate-700"
+        name, email = parseaddr(django_settings.DEFAULT_FROM_EMAIL)
+        env_initials = {
+            "email_from_name": name or "",
+            "email_from_email": email or "",
+            "email_host": django_settings.EMAIL_HOST,
+            "email_port": django_settings.EMAIL_PORT,
+            "email_host_user": django_settings.EMAIL_HOST_USER,
+            "email_use_tls": django_settings.EMAIL_USE_TLS,
+        }
+        for field_name, value in env_initials.items():
+            self.initial[field_name] = value
 
 
 class GroupForm(forms.ModelForm):
